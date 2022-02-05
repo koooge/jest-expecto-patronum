80bbdf27b32a96e4063914fb7e0ccfdec6ef085c
koooge <koooooge@gmail.com>
2022-02-05 03:49:01 +0100

    refatcor: Clean up code
    Signed-off-by: koooge <koooooge@gmail.com>

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const passMessage = (utils, received) => () =>
-  utils.matcherHint('.not.patronum', 'received', '') +
-  '\n\n' +
-  'Expected value to not be odd received:\n' +
-  `  ${utils.printReceived(received)}`;
-
-const failMessage = (utils, received) => () =>
-  utils.matcherHint('.patronum', 'received', '') +
-  '\n\n' +
-  'Expected value to be odd received:\n' +
-  `  ${utils.printReceived(received)}`;
-
 const toPatronum = (expected) => {
   const pass = /^p+a+t+r+o+n+u+m+!*$/i.test(expected);
 
